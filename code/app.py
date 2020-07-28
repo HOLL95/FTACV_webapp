4843d79590c88df2a8c11f01f85e56bc7c33e1f7
Henry Lloyd-Laney <henney@localhost.localdomain>
2020-07-28 12:28:45 +0100

    without debug enable

--- code/app.py
+++ code/app.py
@@ -737,4 +737,4 @@ for experiment in ["ramped", "sinusoidal"]:
         )(plot_harmonics)
 
 if __name__ == '__main__':
-    app.run_server(host='0.0.0.0', port=8050, debug=True)
+    app.run_server(host='0.0.0.0', port=8050, debug=False)
